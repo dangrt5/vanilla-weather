4d37963701d0700e338a8e842a0ab085022fcc0d
dangrt5 <randy.dang05@gmail.com>
2019-01-29 19:50:58 -0800

    cleaned up fetch code

--- main.js
+++ main.js
@@ -14,7 +14,10 @@ function submitButtonHandler(e) {
   e.preventDefault();
   const zipcode = document.querySelector("input").value;
   if (!zipcode) return;
+  getWeatherData(zipcode);
+}
 
+function getWeatherData(zipcode) {
   const weatherData = fetch(
     `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${
       keys.weather_api
@@ -22,6 +25,16 @@ function submitButtonHandler(e) {
     { method: "get" }
   )
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      const { name } = data;
+      const weatherDiv = document.querySelector(".weather-content");
+      const city = document.createElement("h1");
+
+      weatherDiv.innerHTML = "";
+      city.classList.add("text-center");
+      city.textContent = name;
+
+      weatherDiv.appendChild(city);
+    })
     .catch(err => console.log(err));
 }
